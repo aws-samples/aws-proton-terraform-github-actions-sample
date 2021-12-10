ffacbb9d8fcc2da82ef42ccd1199d431c3969032
Alvarez <vallina@dca90483b77d.ant.amazon.com>
2021-12-09 21:03:35 -0800

    updating directory structure

---
+++ sample-templates/sample-vpc-environment-template/v1/infrastructure/config.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" { }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2"
+}
---
+++ sample-templates/sample-vpc-environment-template/v1/infrastructure/vpc.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.environment.inputs.vpc_name
+  cidr = "10.0.0.0/16"
+
+  azs             = [var.environment.inputs.az]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = var.environment.name
+  }
+}

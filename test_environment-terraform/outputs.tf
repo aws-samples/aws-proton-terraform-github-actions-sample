f428b8f86f51753c0c8ae922024a027293c51d6d
sbx_user1051 <sbx_user1051@169.254.49.105>
2021-12-10 05:47:44 +0000

    Update requested for Proton - environment/test_environment-terraform

---
+++ test_environment-terraform/outputs.tf
@@ -0,0 +1,11 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-1:622603276180:environment/test_environment-terraform
+
+If the resource is no longer is accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+output "vpc_arn" {
+    value = module.vpc.vpc_arn
+}

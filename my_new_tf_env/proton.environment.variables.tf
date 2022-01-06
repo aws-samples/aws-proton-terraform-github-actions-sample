bee3dba13f7f4564075d5c069f744b5e93c7b263
sbx_user1051 <sbx_user1051@169.254.19.73>
2022-01-06 20:16:13 +0000

    Update requested for Proton - environment/my_new_tf_env

---
+++ my_new_tf_env/proton.environment.variables.tf
@@ -0,0 +1,16 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-1:617296401743:environment/my_new_tf_env
+
+If the resource is no longer is accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+variable "environment" {
+  type = object({
+    inputs = map(string)
+    name = string
+  })
+  default = null
+}
+
---
+++ my_new_tf_env/vpc.tf
@@ -0,0 +1,27 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-1:617296401743:environment/my_new_tf_env
+
+If the resource is no longer is accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.environment.inputs.vpc_name
+  cidr = "10.0.0.0/16"
+
+  azs             = [var.environment.inputs.az]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = var.environment.name
+  }
+}
+

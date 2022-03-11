c040739d98c8b3396c4b3c4b34d6dcd0aa6e9053
sbx_user1051 <sbx_user1051@169.254.7.173>
2022-03-11 21:22:34 +0000

    Update requested for Proton - environment/my_first_tf_env_2

---
+++ my_first_tf_env_2/config.tf
@@ -0,0 +1,25 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-1:987544922694:environment/my_first_tf_env_2
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {}
+}
+
+# Configure the AWS Provider
+provider "aws" {}
+
+variable "aws_region" {
+  type = string
+}

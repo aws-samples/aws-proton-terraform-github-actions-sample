6670827b1155bcdf0feea79d85f072635de99024
sbx_user1051 <sbx_user1051@169.254.181.253>
2022-03-15 13:23:37 +0000

    Update requested for Proton - environment/tf_test

---
+++ tf_test/proton.environment.variables.tf
@@ -0,0 +1,16 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-1:370926141865:environment/tf_test
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+variable "environment" {
+  type = object({
+    inputs = map(string)
+    name   = string
+  })
+  default = null
+}
+
